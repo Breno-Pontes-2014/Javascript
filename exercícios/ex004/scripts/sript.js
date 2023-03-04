bcbd557b7f2a811258371faff8a505056fa04874
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-03-04 14:38:14 +0000

    ajustes no ex004

--- exercícios/ex004/scripts/sript.js
+++ exercícios/ex004/scripts/sript.js
@@ -1,10 +1,10 @@
-var n1 = Number.parseFloat(window.prompt('Digite um número: '))
-var n2 = Number.parseFloat(window.prompt('Digite um número: '))
+var n1 = Number(window.prompt('Digite um número: '))
+var n2 = Number(window.prompt('Digite um número: '))
 var somar = n1 + n2
 var subtrair = n1 - n2
 var multiplicar = n1 * n2
 var dividir = n1 / n2
-window.alert('A soma dos valores é ' + Number.parseFloat(somar))
-window.alert('A subtração dos valores é ' + Number.parseFloat(subtrair))
-window.alert('A multiplicação dos valores é ' + Number.parseFloat(multiplicar))
-window.alert('A divisão dos valores é ' + Number.parseFloat(dividir))
+window.alert('A soma dos valores é ' + Number(somar))
+window.alert('A subtração dos valores é ' + Number(subtrair))
+window.alert('A multiplicação dos valores é ' + Number(multiplicar))
+window.alert('A divisão dos valores é ' + Number(dividir))
