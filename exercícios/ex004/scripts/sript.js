651b5045eb4582290a0c6358ab8bb1502efb9f4b
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-03-04 15:18:31 +0000

    ajustes

--- exercícios/ex004/scripts/sript.js
+++ exercícios/ex004/scripts/sript.js
@@ -1,4 +1,4 @@
 var n1 = Number(window.prompt('Digite um número: '))
 var n2 = Number(window.prompt('Digite um número: '))
-var somar = eval(n1 + n2)
+var somar = n1 + n2
 window.alert('A soma dos valores é ' + somar)
