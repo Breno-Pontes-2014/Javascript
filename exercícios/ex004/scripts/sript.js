262a18bd8582860c21fcbe5d02e0b46c50d74f82
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-03-04 14:23:35 +0000

    ajuste

--- exercícios/ex004/scripts/sript.js
+++ exercícios/ex004/scripts/sript.js
@@ -4,7 +4,7 @@ var somar = n1 + n2
 var subtrair = n1 - n2
 var multiplicar = n1 * n2
 var dividir = n1 / n2
-window.alert('A Soma dos valores é ' + somar)
-window.alert('A dos valores é ' + subtrair)
-window.alert('A dos valores é ' + multiplicar)
-window.alert('A dos valores é ' + dividir)
+window.alert('A soma dos valores é ' + somar)
+window.alert('A subtração dos valores é ' + subtrair)
+window.alert('A multiplicação dos valores é ' + multiplicar)
+window.alert('A divisão dos valores é ' + dividir)
