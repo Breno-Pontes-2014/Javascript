d6739bfb2d4a3c93e997c0501502c6b91b0705b2
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-03-04 14:21:12 +0000

    ex004

---
+++ exercícios/ex004/scripts/sript.js
@@ -0,0 +1,10 @@
+var n1 = Number.parseInt(window.prompt('Digite um número: '))
+var n2 = Number.parseInt(window.prompt('Digite um número: '))
+var somar = n1 + n2
+var subtrair = n1 - n2
+var multiplicar = n1 * n2
+var dividir = n1 / n2
+window.alert('A Soma dos valores é ' + somar)
+window.alert('A dos valores é ' + subtrair)
+window.alert('A dos valores é ' + multiplicar)
+window.alert('A dos valores é ' + dividir)
