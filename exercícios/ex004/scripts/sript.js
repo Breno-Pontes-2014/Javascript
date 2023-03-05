3c5612f702a93d940dffb1fed0c405258a883463
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-03-05 15:40:07 +0000

    bando de ajuste

--- exercícios/ex004/scripts/sript.js
+++ exercícios/ex004/scripts/sript.js
@@ -1,4 +1,4 @@
 var n1 = Number(window.prompt('Digite um número: '))
 var n2 = Number(window.prompt('Digite um número: '))
 var somar = n1 + n2
-window.alert(`A soma de ${n1} e ${n2} e ${somar}`)
+window.alert(`A soma de ${n1} e ${n2} é ${somar}`)
