e1d381d60c39d230f93c9faa6d6d31d904dc3b8c
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-03-05 15:31:32 +0000

    ex005

---
+++ exercícios/ex005/scripts/sript.js
@@ -0,0 +1,3 @@
+var nome = window.prompt(`Qual é seu nome?`)
+document.write(`<h2>Olá <strong>${nome}</strong>, seu nome tem ${nome.length} letras</h2>`)
+document.write(`<h3>Há, <strong>${nome}</strong>, seu nome em maiúscula é ${nome.toUpperCase()}, e seu nome em minúsculas é ${nome.toLowerCase()}</h3>`)
