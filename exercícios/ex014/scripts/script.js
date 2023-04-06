471f261da0b96e7e12eb8f3758accddd39dabfc2
Breno <113562055+Breno-Pontes-2014@users.noreply.github.com>
2023-04-06 12:25:07 +0000

    ex014

---
+++ exercícios/ex014/scripts/script.js
@@ -0,0 +1,10 @@
+function par_ou_ímpar(n){
+    if (n % 2 == 0){
+        return 'Par!'
+    }
+    else{
+        return 'Ímpar!'
+    }
+}
+console.log(`O número  é...`)
+console.log(par_ou_ímpar(549725482957))
---
+++ exercícios/ex014/scripts/script2.js
@@ -0,0 +1,4 @@
+function somar(n1 = 0,n2 = 0) {
+    return n1 + n2
+}
+console.log(somar(7))
